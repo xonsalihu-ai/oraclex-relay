2f15732ac3e5551be7a09a19f462664be397012a
xonsalihu-ai <xonsalihu@gmail.com>
2026-01-21 12:00:08 +0100

    Update oraclex_relay_v2_updated.js

--- oraclex_relay_v2_updated.js
+++ oraclex_relay_v2_updated.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
